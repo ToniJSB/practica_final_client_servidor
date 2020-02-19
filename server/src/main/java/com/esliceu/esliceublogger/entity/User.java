0371a09b91ef2e861785a41718a508580e7a4eaa
Nico <nicolaslagrega15@gmail.com>
2020-02-19 19:24:57 +0100

    Entity user
    creation of the user entity with hibernate annotations

---
+++ server/src/main/java/com/esliceu/esliceublogger/entity/User.java
@@ -0,0 +1,35 @@
+package com.esliceu.esliceublogger.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private long idUser;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "user_name", unique = true)
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToMany(mappedBy="user")
+    private List<Post> posts;
+
+
+
+}
