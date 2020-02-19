46663e7aef9a0b2ef0fda0be965fa5aa12356ecc
Alexis <alexis.mora.sanchez@gmail.com>
2020-02-19 18:14:22 +0100

    Creating the Structure of the project
    Commit madet by Nico, Toni and Alexis

---
+++ server/src/main/java/com/esliceu/esliceublogger/EsliceubloggerApplication.java
@@ -0,0 +1,13 @@
+package com.esliceu.esliceublogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsliceubloggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsliceubloggerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/esliceu/esliceublogger/EsliceubloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esliceu.esliceublogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsliceubloggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
