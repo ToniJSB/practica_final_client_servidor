42af3349d9fadde6b999213c8eb65d0e3e04ffd3
Toni <Jimenez Sanchez de la Blanca>
2020-02-24 21:44:33 +0100

    PostManager
    Changer interface to class

--- server/src/main/java/com/esliceu/esliceublogger/manager/PostManager.java
+++ server/src/main/java/com/esliceu/esliceublogger/manager/PostManager.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface PostManager {
+public class PostManager {
 
     @Autowired
     PostRepository postDao;
