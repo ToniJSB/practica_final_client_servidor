d69777fcad40a852af3e5027a10f4d385cc0b3cb
Alexis <alexis.mora.sanchez@gmail.com>
2020-03-04 18:05:23 +0100

    Fixed a bug about env variables
    Fixed a bug that caused the environment variables not to be imported

--- overnightJS/src/controller/LoginController.ts
+++ overnightJS/src/controller/LoginController.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Middleware } from '@overnightjs/core';
+import { Controller, Get,Post, Middleware } from '@overnightjs/core';
 import { NextFunction,Request,Response } from 'express';
 const passport = require ('passport')
-
+const googleStrategy = require('./../middleware/googleStrategy')
 @Controller('login')
 export class LoginController {
-    @Get('')
-    @Middleware(passport.authenticate('google'))
+    @Get('google')
+    @Middleware(passport.authenticate('google',{scope:'email'}))
     get(req: Request, res: Response, next: NextFunction): any {
-        console.log(req)
+        console.log("Resposta de get",{
+            req:req,
+            res:res,
+        })
+    }
+
+    @Post('local:username:password')
+    post(req:Request,res:Response,next:NextFunction):any{
+        console.log("Resposta de post",{
+            req:req,
+            res:res,
+        })
     }
 }
--- overnightJS/src/middleware/googleStrategy.ts
+++ overnightJS/src/middleware/googleStrategy.ts
@@ -1,18 +1,21 @@
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const dotenv = require('./../../.env')
+var passport = require('passport')
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+require('../config/environment')
 // Use the GoogleStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
 passport.use(new GoogleStrategy({
-    clientID: dotenv.GOOGLE_CLIENT_ID,
-    clientSecret: dotenv.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
-  },
- /* function(accessToken:any, refreshToken:any, profile:any, done:any) {
-     //  User.findOrCreate({ googleId: profile.id }, function (err:any, user:any) {
-       //  return done(err, user);
-       });
-  }*/
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:8080/posts"
+},
+  function (accessToken: any, refreshToken: any, profile: any, done: any) {
+    console.log({
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+      profile: profile,
+      done: done
+    })
+  }
 ))
