97813afb270d6e097c03d57489d39876405d46e8
Toni <Jimenez Sanchez de la Blanca>
2020-03-08 18:10:48 +0100

    Auto stash before merge of "Development" and "origin/Development"

--- server/src/main/java/com/esliceu/esliceublogger/controller/PostFormController.java
+++ server/src/main/java/com/esliceu/esliceublogger/controller/PostFormController.java
@@ -7,10 +7,10 @@ import com.esliceu.esliceublogger.manager.PostManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.google.gson.Gson;
+
+import java.util.IllegalFormatCodePointException;
 
 @RestController
 public class PostFormController {
@@ -23,26 +23,44 @@ public class PostFormController {
 
     @GetMapping("/postForm")
     public ResponseEntity<String> postForm(@RequestParam(name = "id") String id) {
-        Post postGetted = postManager.getByIdPost(id);
-        Post newPost = new Post();
 
-        newPost.setTitle(postGetted.getTitle());
-        newPost.setContent(postGetted.getContent());
-        newPost.setLangOriginal(postGetted.getLangOriginal());
-        newPost.setLangTranslate(postGetted.getLangTranslate());
-        newPost.setDate(postGetted.getDate());
-        User user = new User();
-        user.setIdUser(postGetted.getAuthor().getIdUser());
-        user.setEmail(postGetted.getAuthor().getEmail());
-        user.setFirstName(postGetted.getAuthor().getFirstName());
-        user.setLastName(postGetted.getAuthor().getLastName());
-        user.setPassword(postGetted.getAuthor().getPassword());
-        user.setUserName(postGetted.getAuthor().getUserName());
- 
-        newPost.setAuthor(user);
-        String jsonObjsect = gson.toJson(newPost);
-        return new ResponseEntity<>(jsonObjsect, HttpStatus.OK);
+        Post postGetted = postManager.getByIdPost(id);
+        if (postGetted != null) {
+
+            Post newPost = new Post();
+            newPost.setTitle(postGetted.getTitle());
+            newPost.setContent(postGetted.getContent());
+            newPost.setLangOriginal(postGetted.getLangOriginal());
+            newPost.setLangTranslate(postGetted.getLangTranslate());
+            newPost.setDate(postGetted.getDate());
+
+            User user = new User();
+            user.setEmail(postGetted.getAuthor().getEmail());
+            newPost.setAuthor(user);
+
+            String jsonObjsect = gson.toJson(newPost);
+            return new ResponseEntity<>(jsonObjsect, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("POST NOT FOUND" ,HttpStatus.NOT_FOUND);
     }
 
+    @DeleteMapping("/deletePost")
+    public ResponseEntity<String> deletePost(@RequestParam(name = "idPost") String idPost) {
+        postManager.delete(postManager.getByIdPost(idPost));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
+    @PutMapping("/UpdatePost")
+    public ResponseEntity<String> updatePost(
+            @RequestParam(name = "idPost") String idPost,
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "content") String content,
+            @RequestParam(name = "langOriginal") String langOriginal,
+            @RequestParam(name = "langTranslate") String langTranslate) {
+
+        postManager.save(
+                new Post()
+        );
+    }
 }
+
