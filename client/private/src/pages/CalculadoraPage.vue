9b0c7ffa4ef9f3b2b42ecdf33f6b709d048dc875
Toni <Jimenez Sanchez de la Blanca>
2020-03-09 15:16:46 +0100

    Form Calculator done. without functionallity.

---
+++ client/private/src/pages/CalculadoraPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <CalculadoraComponent></CalculadoraComponent>
+</template>
+
+<script>
+import CalculadoraComponent from '../components/CalculadoraComponent';
+export default {
+    name: 'CalculadoraPage',
+    components: {
+        CalculadoraComponent
+    },
+
+
+}
+</script>
